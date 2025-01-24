426d8ba8c4626469f4119382935a35de98f80a5b
luminiksi <luminiksi@ya.ru>
2025-01-24 15:43:01 +0300

    Практика к уроку - 2.5

---
+++ skillboxJava/src/hometasks/practics/part2/lesson5/Task2.java
@@ -0,0 +1,13 @@
+package hometasks.practics.part2.lesson5;
+
+import myFeatures.Printer;
+
+public class Task2 {
+    public static void run() {Printer.printLessonNumber(3, 2);
+        Printer.printPracticeInfo();
+        Printer.printTaskNumber(2);
+        Printer.printDescription("В предыдущем задании оказывется уже упрастила... %) ");
+
+    }
+
+}
