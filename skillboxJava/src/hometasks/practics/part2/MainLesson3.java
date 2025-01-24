3f788b8077fddf99b812a1eaad5e82f6addc1acb
luminiksi <luminiksi@ya.ru>
2025-01-24 15:23:42 +0300

    Практика к уроку - 2.4

--- skillboxJava/src/hometasks/practics/part2/MainLesson3.java
+++ skillboxJava/src/hometasks/practics/part2/MainLesson3.java
@@ -2,7 +2,7 @@ package hometasks.practics.part2;
 
 import hometasks.practics.part2.lesson3.*;
 
-public class Main {
+public class MainLesson3 {
     public static void main(String[] args) {
         Task1.run();
         System.out.println();
