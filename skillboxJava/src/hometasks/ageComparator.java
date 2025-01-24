a16739c97fbab8204d92cea4151a6b2096bf861f
luminiksi <luminiksi@ya.ru>
2025-01-24 16:05:38 +0300

    Практика 2.12

---
+++ skillboxJava/src/hometasks/ageComparator.java
@@ -0,0 +1,10 @@
+package hometasks;
+
+import hometasks.lesson2.Task2;
+
+public class ageComparator {
+    public static void main(String[] args) {
+        Task2.run();
+        System.out.println(true && (false || (false || true)));
+    }
+}
