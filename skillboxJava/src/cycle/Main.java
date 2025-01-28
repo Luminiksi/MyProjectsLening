2247015f4985f23677e20c08bdc47c06b9ec1a8e
luminiksi <luminiksi@ya.ru>
2025-01-28 10:27:13 +0300

    Lesson 3.4

--- skillboxJava/src/cycle/Main.java
+++ skillboxJava/src/cycle/Main.java
@@ -4,9 +4,12 @@ import cycle.lessons.*;
 
 public class Main {
     public static void main(String[] args) {
-//        Lesson2.run();
+        Lesson2.run();
         System.out.println();
         System.out.println();
         Lesson3.run();
+        System.out.println();
+        System.out.println();
+        Lesson4.run();
     }
 }
