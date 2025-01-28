1ed8e60848eed694eb8853f7bf0036b423743aee
luminiksi <luminiksi@ya.ru>
2025-01-28 15:34:28 +0300

    Практика 3.10

---
+++ skillboxJava/src/hometasks/FactorialCalculator.java
@@ -0,0 +1,14 @@
+package hometasks;
+
+import java.util.Scanner;
+
+public class FactorialCalculator {
+    public static void main(String[] args) {
+        int value = new Scanner(System.in).nextInt();
+        int fact = 1;
+        for (int i = 1; i <= value; i++) {
+            fact *= i;
+        }
+        System.out.println("Факториал " + value + "! равен " + fact);
+    }
+}
