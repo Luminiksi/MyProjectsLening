54cd9aa8fa595afd9505462cdcb666fc14e5910e
luminiksi <luminiksi@ya.ru>
2025-01-29 09:17:52 +0300

    4.4 Возвращаемые значения (практика)

---
+++ skillboxJava/src/hometasks/practics/part4/Methods5.java
@@ -0,0 +1,12 @@
+package hometasks.practics.part4;
+
+public class Methods5 {
+    public static void main(String[] args) {
+        int sum = calculateSum(300, 200, 700);
+        System.out.println(sum);
+    }
+    public static int calculateSum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
