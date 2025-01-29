5273e45ca187dc2ff85f0b87366eeccfd8bd96e7
luminiksi <luminiksi@ya.ru>
2025-01-29 12:42:15 +0300

    5.4 Records в Java 17 (из урока)

---
+++ skillboxJava/src/other/Student.java
@@ -0,0 +1,23 @@
+package other;
+
+record Student(String name, int age) {
+
+    private static final int adultAge = 18;
+
+    public Student{
+        if (age < 0) {
+            System.out.println("Вы создали студента с отрицательным возрастом!");
+        }
+    }
+    public Student (String name) {
+        this(name, 18);
+    }
+
+    public boolean isAdult() {
+        return age >= adultAge;
+    }
+
+    public static Student createAdultStudent(String name){
+        return new Student(name, adultAge);
+    }
+}
