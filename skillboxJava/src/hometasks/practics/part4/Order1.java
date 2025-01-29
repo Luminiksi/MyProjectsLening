8fbaa9c39631a7746a260fb0732dd261c21cd311
luminiksi <luminiksi@ya.ru>
2025-01-29 09:31:56 +0300

    4.5 Классы и объекты (практика)

---
+++ skillboxJava/src/hometasks/practics/part4/Order1.java
@@ -0,0 +1,13 @@
+package hometasks.practics.part4;
+
+public class Order1 {
+    private int count = 0;
+
+    public void addProducts(int productsCount) {
+        count = count + productsCount;
+    }
+
+    public int getProductsCount() {
+        return count;
+    }
+}
