a218b49b0d2d00b26b472f2b70f0b39d22270d91
luminiksi <luminiksi@ya.ru>
2025-01-29 11:34:05 +0300

    5.2 Инкапсуляция (из урока)

---
+++ skillboxJava/src/driverlessCar/Main.java
@@ -0,0 +1,9 @@
+package driverlessCar;
+
+public class Main {
+    public static void main(String[] args) {
+        GearBox gearBox = new GearBox();
+        gearBox.getCurrentGear();
+        gearBox.shiftDown();
+    }
+}
