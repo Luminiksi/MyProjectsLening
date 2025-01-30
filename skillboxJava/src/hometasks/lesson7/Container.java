3fc0a7d945d19b5e2a9c79b89da253c8fa37c80e
luminiksi <luminiksi@ya.ru>
2025-01-30 15:57:01 +0300

    7.9 Практическая работа (Задание + заготовка на практику)

---
+++ skillboxJava/src/hometasks/lesson7/Container.java
@@ -0,0 +1,13 @@
+package hometasks.lesson7;
+
+public class Container {
+    private Integer count;
+
+    public void addCount(int value) {
+        count = count + value;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
