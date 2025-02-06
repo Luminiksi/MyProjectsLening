0f0d1afaf5fe11735da14012f98559f380a7706a
luminiksi <luminiksi@ya.ru>
2025-02-06 14:43:01 +0300

    9.5 Методы работы с подстроками (из урока)

--- skillboxJava/src/strings/Main.java
+++ skillboxJava/src/strings/Main.java
@@ -2,10 +2,12 @@ package strings;
 
 public class Main {
     public static void main(String[] args) {
-        Lesson2.run();
+        /*Lesson2.run();
         System.out.println();
         Lesson3.run();
         System.out.println();
         Lesson4.run();
+        System.out.println();*/
+        Lesson5.run();
     }
 }
