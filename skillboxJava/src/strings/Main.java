5848689152919fa776fc0c6f8ad88753a3ad1933
luminiksi <luminiksi@ya.ru>
2025-02-18 13:22:32 +0300

    9.7 Регулярные выражения(из урока)

--- skillboxJava/src/strings/Main.java
+++ skillboxJava/src/strings/Main.java
@@ -2,12 +2,14 @@ package strings;
 
 public class Main {
     public static void main(String[] args) {
-        /*Lesson2.run();
+        Lesson2.run();
         System.out.println();
         Lesson3.run();
         System.out.println();
         Lesson4.run();
-        System.out.println();*/
+        System.out.println();
         Lesson5.run();
+        System.out.println();
+        Lesson7.run();
     }
 }
