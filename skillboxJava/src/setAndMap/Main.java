e175fe442e9211b0fddc5c6c9410eb3a8294f79d
luminiksi <luminiksi@ya.ru>
2025-02-27 13:55:11 +0300

    11.7 Коллекции HashMap и TreeMap - из урока

--- skillboxJava/src/setAndMap/Main.java
+++ skillboxJava/src/setAndMap/Main.java
@@ -2,11 +2,13 @@ package setAndMap;
 
 import setAndMap.lesson3.Lesson3;
 import setAndMap.lesson4.Lesson4;
+import setAndMap.lesson7.Lesson7;
 
 public class Main {
     public static void main(String[] args) {
         Lesson2.run();
         Lesson3.run();
         Lesson4.run();
+        Lesson7.run();
     }
 }
---
+++ skillboxJava/src/setAndMap/lesson7/CarColor.java
@@ -0,0 +1,8 @@
+package setAndMap.lesson7;
+
+public enum CarColor {
+    WHITE,
+    BLACK,
+    BLUE,
+    RED
+}
