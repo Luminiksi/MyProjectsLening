d025349528e9e6f930d8520940dc9d5760a90414
luminiksi <luminiksi@ya.ru>
2025-02-27 11:50:12 +0300

    11.3 Механизм работы HashSet - мз урока

--- skillboxJava/src/setAndMap/Main.java
+++ skillboxJava/src/setAndMap/Main.java
@@ -1,7 +1,10 @@
 package setAndMap;
 
+import setAndMap.lesson3.Lesson3;
+
 public class Main {
     public static void main(String[] args) {
         Lesson2.run();
+        Lesson3.run();
     }
 }
