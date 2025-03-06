7eaec860e87ecd588ae1e1eb4f1ea33f94086dcc
luminiksi <luminiksi@ya.ru>
2025-03-06 15:14:46 +0300

    13.8 Практическая работа (автотесты) - решение

--- skillboxPractics/java-inheritance-basics/src/main/java/practice/CardAccount.java
+++ skillboxPractics/java-inheritance-basics/src/main/java/practice/CardAccount.java
@@ -3,4 +3,11 @@ package practice;
 public class CardAccount extends BankAccount {
     // не забывайте, обращаться к методам и конструкторам родителя
     // необходимо используя super, например, super.put(10D);
+
+
+    @Override
+    public void take(double amountToTake) {
+        amountToTake *= 1.01;
+        super.take(amountToTake);
+    }
 }
--- skillboxPractics/java-inheritance-basics/src/main/java/practice/DepositAccount.java
+++ skillboxPractics/java-inheritance-basics/src/main/java/practice/DepositAccount.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class DepositAccount extends BankAccount {
+import java.time.LocalDate;
 
+public class DepositAccount extends BankAccount {
+    private LocalDate lastIncome;
+
+    @Override
+    public void put(double amountToPut) {
+        super.put(amountToPut);
+        lastIncome = LocalDate.now();
+    }
+
+    @Override
+    public void take(double amountToTake) {
+        if (LocalDate.now().isBefore(lastIncome.plusMonths(1))) {
+            return;
+        }
+        super.take(amountToTake);
+    }
 }
