4a6ad0fa7b723e1f5bf88778d1c0d2beadf87f57
luminiksi <luminiksi@ya.ru>
2025-03-28 13:03:35 +0300

    17.4 Терминальные операторы - из урока

--- skillboxJava/src/streamAPI/Main.java
+++ skillboxJava/src/streamAPI/Main.java
@@ -1,8 +1,9 @@
 package streamAPI;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Lesson2.run();
         Lesson3.run();
+        Lesson4.run();
     }
 }
