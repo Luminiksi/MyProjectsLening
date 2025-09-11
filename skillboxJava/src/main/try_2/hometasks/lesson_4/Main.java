7fe4384a52d5a6c2d5de98c71c7c652c07907717
luminiksi <luminiksi@ya.ru>
2025-09-11 10:28:03 +0300

    4.11 Практическая работа (задания и заготовка)

---
+++ skillboxJava/src/main/try_2/hometasks/lesson_4/Main.java
@@ -0,0 +1,10 @@
+package main.try_2.hometasks.lesson_4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Basket basket = new Basket();
+        basket.add("Milk", 40);
+        basket.print("Milk");
+    }
+}
