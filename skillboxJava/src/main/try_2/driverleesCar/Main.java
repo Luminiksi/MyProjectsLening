05dd84664f12697da3b69d6db81e430b6a9841dc
luminiksi <luminiksi@ya.ru>
2025-09-17 13:12:41 +0300

    5.2 Инкапсуляция

---
+++ skillboxJava/src/main/try_2/driverleesCar/Main.java
@@ -0,0 +1,8 @@
+package main.try_2.driverleesCar;
+
+public class Main {
+    public static void main(String[] args) {
+        GearBox gearBox = new GearBox();
+        System.out.println(gearBox.getCurrentGear());
+    }
+}
---
+++ skillboxJava/src/main/try_2/hometasks/Lesson_5/encapsulationExperiments/ChildBankAccount.java
@@ -0,0 +1,41 @@
+package main.try_2.hometasks.Lesson_5.encapsulationExperiments;
+
+public class ChildBankAccount {
+    private double balance;
+    private double maxBalance;
+
+    public ChildBankAccount(int maxBalance) {
+        this.maxBalance = maxBalance;
+        balance = 0;
+    }
+
+    public boolean depositMoney(double value) {
+        if (value < 0) {
+            return false;
+        }
+        double newBalance = balance + value;
+        if (newBalance > maxBalance) {
+            return false;
+        }
+        balance = newBalance;
+        return true;
+    }
+
+    public boolean debitMoney(double value) {
+        if (value < 0) {
+            return false;
+        }
+        double newBalance = balance - value;
+        if (newBalance < 0) {
+            return false;
+        }
+        balance = newBalance;
+        return true;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+}
