1a9a6cff7d790356a55ebf987064b269a24019ec
luminiksi <luminiksi@ya.ru>
2025-12-03 15:47:39 +0300

    7.9 Итоги пройденных тем. Проверьте себя (2 задача + решение)

--- skillboxJava/src/main/try_2/primitives/IntOrInteger/Main.java
+++ skillboxJava/src/main/try_2/primitives/IntOrInteger/Main.java
@@ -1,6 +1,8 @@
 package main.try_2.primitives.IntOrInteger;
 
 public class Main {
+    public static final int MAX_CHAR = 65536;
+
     public static void main(String[] args) {
         Container container = new Container(0);
         container.addCount(5672);
@@ -10,5 +12,20 @@ public class Main {
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
+//        for (int i = 1040; i <= 1105; i++) {
+//            char c = (char) i;
+//            System.out.println(i + " - " +  c);
+//        }
+//        int i = 1025;
+//        char c = (char) i;
+//        System.out.println(i + " - " +  c);
+
+        for (int i = 0; i <= MAX_CHAR; i++) {
+            if ((char) i == 'Ё' || (char) i == 'ё') {
+                System.out.println((char) i);
+            } else if ((char) i >= 'А' && (char) i <= 'я') {
+                System.out.println((char) i);
+            }
+        }
     }
 }
