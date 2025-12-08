dbddf6b14a0ddb8b2e1872e7ba3924ea03746558
luminiksi <luminiksi@ya.ru>
2025-12-08 12:48:16 +0300

    8.10 Практическая работа (автотесты) - решение

--- skillboxPractics_2/java_NumbersAndDates/src/main/java/practice/Birthdays.java
+++ skillboxPractics_2/java_NumbersAndDates/src/main/java/practice/Birthdays.java
@@ -1,5 +1,11 @@
 package practice;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 public class Birthdays {
 
     public static void main(String[] args) {
@@ -13,11 +19,29 @@ public class Birthdays {
     }
 
     public static String collectBirthdays(int year, int month, int day) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy - EEE").localizedBy(new Locale("us"));;
+        StringBuilder birthdays = new StringBuilder();
+        LocalDate now = LocalDate.now();
+        for (int i = 0; i <= now.getYear(); i++) {
+            int newYear = year + i;
+            LocalDate birthday = LocalDate.of(newYear, month, day);
+            if (birthday.isAfter(now)) {
+                break;
+            }
+            if (!birthdays.isEmpty()) {
+                birthdays.append("\n");
+            }
+
+//            DayOfWeek dayOfWeek = birthday.getDayOfWeek();
+//            birthdays.append(i + " - " + formatter.format(birthday) + " - " + dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.ENGLISH));
+            birthdays.append(i + " - " + formatter.format(birthday));
+        }
+
 
         //TODO реализуйте метод для построения строки в следующем виде
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
 
-        return "";
+        return birthdays.toString();
     }
 }
--- skillboxPractics_2/java_NumbersAndDates/src/main/java/practice/TrucksAndContainers.java
+++ skillboxPractics_2/java_NumbersAndDates/src/main/java/practice/TrucksAndContainers.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class TrucksAndContainers {
 
+    public static final int MAX_BOXES_IN_CONTAINER = 27;
+    public static final int MAX_CONTAINERS_IN_TRUCK = 12;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -11,6 +14,28 @@ public class TrucksAndContainers {
         int boxes = scanner.nextInt();
 
         // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
+        int containers = boxes / MAX_BOXES_IN_CONTAINER + (boxes % MAX_BOXES_IN_CONTAINER == 0 ? 0 : 1);
+        int trucks = containers / MAX_CONTAINERS_IN_TRUCK + (containers % MAX_CONTAINERS_IN_TRUCK == 0 ? 0 : 1);
+
+        int containersCount = 1;
+        int boxesCount = 1;
+
+        for (int indexTruck = 1; indexTruck <= trucks; indexTruck++) {
+            System.out.println("Грузовик: " + indexTruck);
+            for (int conteinerInTruckIndex = 0; conteinerInTruckIndex < MAX_CONTAINERS_IN_TRUCK; conteinerInTruckIndex++) {
+                System.out.println("    Контейнер: " + containersCount++);
+                for (int boxInContainerIndex = 0; boxInContainerIndex < MAX_BOXES_IN_CONTAINER; boxInContainerIndex++) {
+                    System.out.println("        Ящик: " + boxesCount++);
+                    if (boxesCount > boxes) {
+                        break;
+                    }
+                }
+                if (containersCount > containers) {
+                    break;
+                }
+            }
+        }
+
         // пример вывода при вводе 2
         // для отступа используйте табуляцию - \t
 
@@ -23,6 +48,8 @@ public class TrucksAndContainers {
         грузовиков - 1 шт.
         контейнеров - 1 шт.
         */
+
+
     }
 
 }
